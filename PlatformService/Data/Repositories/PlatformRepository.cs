8db58648f2163b14673497331ba03b3e0263e369
Владимир Старовойт <starovoyt_1995@mail.ru>
2023-02-20 14:19:33 +0300

    Добавьте файлы проекта.

---
+++ PlatformService/Data/Repositories/PlatformRepository.cs
@@ -0,0 +1,38 @@
+﻿using PlatformService.Data.Interfaces;
+using PlatformService.Models;
+
+namespace PlatformService.Data.Repositories
+{
+    public class PlatformRepository : IPlatformRepo
+    {
+        private readonly AppDbContext _context;
+
+        public PlatformRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            if(platform == null)
+                throw new ArgumentNullException();
+
+            _context.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return _context.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+    }
+}
---
+++ PlatformService/Initializer.cs
@@ -0,0 +1,13 @@
+﻿using PlatformService.Data.Interfaces;
+using PlatformService.Data.Repositories;
+
+namespace PlatformService
+{
+    public static class Initializer
+    {
+        public static void InitializeRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IPlatformRepo, PlatformRepository>();
+        }
+    }
+}
